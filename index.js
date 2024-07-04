1dd5b0bdab3d92010a88e8d5c23612221f7db2b8
santinolargayoli90 <166016530+santinolargayoli90@users.noreply.github.com>
2024-07-04 07:48:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//Actividad #1: 
+
+for (let num = 10; num > 0; num = num - 1) {
+
+    console.log(num)
+}
+//Actividad #2:
+for (let num2 = 15; num2 > 4; num2 = num2-1) {
+    console.log(num2)
+    
+}
+// Actividad #3: 
+for (let num3 = 1; num3 < 100; num3 = num3+2) {
+    console.log(num3);
+    
+}
+// Actividad #4: 
+
+for (let num4 = 1; num4 < 11; num4 = num4 + 1) {
+    let resultado = 7*num4
+    console.log(resultado)
+    
+}
